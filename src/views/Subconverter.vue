72cfae6bd14dca1a0bfa9a13fdef0e9fc3b35247
clashheroku <96043807+clashheroku@users.noreply.github.com>
2021-12-13 20:46:31 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://clashv2s.clashv2rss.workers.dev/sub?" }],
+        backendOptions: [{ value: "https://clashv2s.clashv2rss.workers.dev/config sub?" }],
         remoteConfig: [
         {
             label: "ACL4SSR",
